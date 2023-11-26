2a4acf390c82ab265c3ca46d59ae2e03ba427557
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2023-11-26 13:59:52 +0300

    Create seperate-with-asterisks.py

---
+++ input-output/seperate-with-asterisks.py
@@ -0,0 +1,9 @@
+# Program to accept three strings from the user and seperate them with **
+
+print("This program will read three strings from the user and seperate them with **.")
+
+str1 = input("Enter the first string  : ")
+str2 = input("Enter the second string : ")
+str3 = input("Enter the third string  : ")
+
+print(str1,str2,str3, sep='**')
