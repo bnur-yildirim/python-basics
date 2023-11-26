915295dd71b71bf4e3fd2856acb04dacb43b4504
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2023-11-26 13:53:45 +0300

    Create multiplication-of-two-numbers.py

---
+++ input-output/multiplication-of-two-numbers.py
@@ -0,0 +1,8 @@
+# Program to accept two numbers from the user and calculate multiplication
+
+print("This program will read two integers and display the multiplication.")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Multiplication is :",num1*num2)
