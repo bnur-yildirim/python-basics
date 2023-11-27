448b8208e7e0936b54970fe277bbd7ec279d9dbb
bnur-yildirim <128072981+bnur-yildirim@users.noreply.github.com>
2023-11-27 08:33:09 +0300

    Create decimal-to-octal.py

---
+++ input-output/decimal-to-octal.py
@@ -0,0 +1,7 @@
+# Program to convert decimal to octal
+
+print("This program will convert decimal to octal.")
+
+decimalNum = int(input("Enter a decimal number :"))
+
+print("Octal value is: %o"%decimalNum)
