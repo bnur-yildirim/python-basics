d74b895180c51e9105de253c740b53ea479918dd
Beyza Nur YILDIRIM <nury57909@gmail.com>
2024-01-25 10:15:09 +0300

    Updated online-shop.py

--- online-shop.py
+++ online-shop.py
@@ -1,24 +1,44 @@
 # function that checks the stocks for the items customer wants,
 # if stock isn't enough, amount in the shopping list is updated 
 # to the highest amount possible
-def check_stock(inventory, SList):
-    newList = []
-    for key in inventory:
-        for item, amount in SList:
-            if item == key:
-                if inventory[key][1] == 0:
-                    continue
-                elif amount > inventory[key][1]:
-                    newList.append((item, inventory[key][1]))
+def check_stock(inventory, shopping_list):
+    new_list = []
+    for item, amount in shopping_list:              # item = item name, amount = amount customer wants
+        stock = inventory[item][1]
+        if item in inventory:                       # if we find item in the inventory
+            if stock == 0:                          # if stock of the item is zero
+                continue
+            elif amount > stock:                    # if stock is not enough
+                new_amount = int(input(f"Stock of {item} is only {stock}.\nEnter a new amount: "))
+
+                # make sure new amount is entered correctly
+                while new_amount < 0 or new_amount > stock:
+                    new_amount = int(input(f"Amount should be between (0 - {stock}).\nEnter a new amount: ")) 
+                if new_amount == 0:               # if customer doesn't want the item anymore
+                    continue                      # this line will remove it from the shopping list
                 else:
-                    newList.append((item, amount))
-    return newList
+                    new_list.append((item, new_amount))
+            else:
+                new_list.append((item, amount))                                   
+    return new_list
+
+#calculate the total bill
+def compute_bill(inventory, shopping_list):
+    bill = 0
+    for item, amount in shopping_list:           # go through each item in the shopping list
+        bill += inventory[item][0] * amount      
+    return bill
+
+def update_stock(inventory, shopping_list):
+    for item, amount in shopping_list:
+        inventory[item][1] -= amount            # decrease the stock with the amount purchased
+    return inventory
 
 #inventory of the online shop
 # inventory = {"item name":[price, stock, "section the item belongs"]}
 inventory = {"Laptop":[1200.00, 10, "Electronics"],
              "Coffee Maker":[50.99, 20, "Appliances"],
-             "Running Shooes":[79.95, 30, "Clothing"],
+             "Running Shoes":[79.95, 30, "Clothing"],
              "Smartphone":[899.99, 15, "Electronics"],
              "Cookware Set":[129.50, 25, "Kitchen"],
              "Backpack":[34.99, 40, "Accessories"],
@@ -27,9 +47,15 @@ inventory = {"Laptop":[1200.00, 10, "Electronics"],
 
 # shopping list of the customer
 # shopping_list = [("item name", amount)]
-shoppingList = [("Laptop", 1), ("Coffee Maker", 2), ("Smartphone", 1),
-                ("Cookware Set", 1), ("Backpack", 90), ("LED TV", 1),
+shopping_list = [("Laptop", 1), ("Coffee Maker", 30), ("Smartphone", 1),
+                ("Cookware Set", 1), ("Backpack", 2), ("LED TV", 1),
                 ("T-Shirt", 3)]
-print(shoppingList)
-shoppingList = check_stock(inventory, shoppingList)
-print(shoppingList)
+
+shopping_list = check_stock(inventory, shopping_list)
+print(f"Your updated shopping list is:\n{shopping_list}\n")
+
+bill = compute_bill(inventory, shopping_list)
+print(f"Total price: $ {bill:.2f}\n")
+
+inventory = update_stock(inventory, shopping_list)
+print(f"The inventory has been updated:\n{inventory}\n")
